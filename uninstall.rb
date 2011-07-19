c1edfe9a27c0e3967036b816945e4bd8675164b8
David Cabo <david@calibea.com>
2011-07-19 15:16:41 +0200

    improve uninstall process to handle missing symlink

--- uninstall.rb
+++ uninstall.rb
@@ -1,5 +1,8 @@
 # Uninstall hook code here
 
 main_app_path = File.join(RAILS_ROOT, 'public', 'asktheeu-theme')
-puts "Deleting symbolink link at #{main_app_path}..."
-puts `rm #{main_app_path}`
+if File.exists?(main_app_path) && File.symlink?(main_app_path)
+	print "Deleting symbolink link at #{main_app_path}... "
+	puts `rm #{main_app_path}`
+	puts "done"
+end
