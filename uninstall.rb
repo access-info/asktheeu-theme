4aad6fe44e1783d8e8f7963a50663afc633e490f
David Cabo <david@calibea.com>
2011-07-19 15:29:36 +0200

    use File.delete while uninstalling instead of external shell

--- uninstall.rb
+++ uninstall.rb
@@ -3,6 +3,6 @@
 main_app_path = File.join(RAILS_ROOT, 'public', 'asktheeu-theme')
 if File.exists?(main_app_path) && File.symlink?(main_app_path)
 	print "Deleting symbolink link at #{main_app_path}... "
-	puts `rm #{main_app_path}`
+	File.delete(main_app_path)
 	puts "done"
 end
