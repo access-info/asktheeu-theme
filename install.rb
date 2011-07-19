841c71e98218fbc73414da62cee9705abf6e364c
David Cabo <david@calibea.com>
2011-07-19 04:43:03 +0200

    test

--- install.rb
+++ install.rb
@@ -3,5 +3,6 @@
 #if RAILS_MODE == 'developemnt' do 
 	full_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
 
+puts "=======#{Rails.configuration.root_path}"
 	# TODO: check for existence
 	puts `ln -s #{full_path} ../../../public/asktheeu`
