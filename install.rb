da0e1e88f72bafeedbc1a247d1dda952de993f2e
David Cabo <david@calibea.com>
2011-07-19 05:01:25 +0200

    improving install/uninstall process

--- install.rb
+++ install.rb
@@ -1,5 +1,7 @@
 # Install hook code here
 
-full_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
+plugin_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
+main_app_path = File.join(RAILS_ROOT, 'public', 'asktheeu-theme')
 
-puts `ln -s #{full_path} #{File.join(RAILS_ROOT, 'public', 'asktheeu-theme')}`
+puts "Creating symbolink link from #{main_app_path} to #{plugin_path}..."
+puts `ln -s #{plugin_path} #{main_app_path}`
--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,5 @@
 # Uninstall hook code here
+
+main_app_path = File.join(RAILS_ROOT, 'public', 'asktheeu-theme')
+puts "Deleting symbolink link at #{main_app_path}..."
+puts `rm #{main_app_path}`
