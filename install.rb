a526bdaa0ff8f6989703487eaecd1ac272307a1a
David Cabo <david@calibea.com>
2011-07-19 04:56:24 +0200

    cleaning up comments

--- install.rb
+++ install.rb
@@ -1,7 +1,5 @@
 # Install hook code here
 
-#if RAILS_MODE == 'developemnt' do 
-	full_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
+full_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
 
-	# TODO: check for existence
-	puts `ln -s #{full_path} #{File.join(RAILS_ROOT, 'public', 'asktheeu-theme')}`
+puts `ln -s #{full_path} #{File.join(RAILS_ROOT, 'public', 'asktheeu-theme')}`
