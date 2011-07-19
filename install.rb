4cf13192924b000c4bee26229d96f9338de13003
David Cabo <david@calibea.com>
2011-07-19 04:53:40 +0200

    quick logo test

--- install.rb
+++ install.rb
@@ -4,4 +4,4 @@
 	full_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
 
 	# TODO: check for existence
-	puts `ln -s #{full_path} #{File.join(RAILS_ROOT, 'public', 'asktheeu')}`
+	puts `ln -s #{full_path} #{File.join(RAILS_ROOT, 'public', 'asktheeu-theme')}`
