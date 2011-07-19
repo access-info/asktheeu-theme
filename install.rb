8193bfb53a83b6be1532ea51112ba64d0ef672c0
David Cabo <david@calibea.com>
2011-07-19 15:25:35 +0200

    improve install process to handle existing symlink

--- install.rb
+++ install.rb
@@ -1,16 +1,12 @@
 # Install hook code here
 
-begin
-	plugin_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
-	main_app_path = File.join(RAILS_ROOT, 'public', 'asktheeu-theme')
+plugin_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
+main_app_path = File.join(RAILS_ROOT, 'public', 'asktheeu-theme')
 
-	# If the symlink to be created exists, warn the user and do nothing
-	if File.exists?(main_app_path)
-		puts "WARNING: #{main_app_path} already exists, the symbolic link won't be created"
-		return
-	end
-	
-	# Create symlink
+# If the symlink to be created exists, warn the user and do nothing
+if File.exists?(main_app_path)
+	puts "WARNING: #{main_app_path} already exists, the symbolic link won't be created"
+else # Create symlink
 	print "Creating symbolink link from #{main_app_path} to #{plugin_path}... "
 	File.symlink(plugin_path, main_app_path)
 	puts "done"
