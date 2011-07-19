57b92190c5aac5da4cc047c5dfb12b7e60d7b24a
David Cabo <david@calibea.com>
2011-07-19 04:45:06 +0200

    test

--- install.rb
+++ install.rb
@@ -3,6 +3,5 @@
 #if RAILS_MODE == 'developemnt' do 
 	full_path = File.expand_path(File.join(File.dirname(__FILE__), "public"))
 
-puts "=======#{RAILS_ROOT}"
 	# TODO: check for existence
-	puts `ln -s #{full_path} ../../../public/asktheeu`
+	puts `ln -s #{full_path} #{File.join(RAILS_ROOT, 'public', 'asktheeu')}`
