31f5f7f0033304abbe9aa1023c30126c387e8197
David Cabo <david@calibea.com>
2011-07-28 21:06:40 +0200

    Override public body categories with a draft of EU ones

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -5,3 +5,12 @@ class ActionController::Base
         self.prepend_view_path File.join(File.dirname(__FILE__), "views")
     end
 end
+
+# In order to have the theme lib/ folder ahead of the main app one,
+# inspired in Ruby Guides explanation: http://guides.rubyonrails.org/plugins.html
+%w{ lib }.each do |dir|
+  path = File.join(File.dirname(__FILE__), dir)
+  $LOAD_PATH.insert(0, path)
+  ActiveSupport::Dependencies.load_paths << path
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
+end
---
+++ lib/lib/public_body_categories_en.rb
@@ -0,0 +1,26 @@
+# lib/public_body_categories.rb:
+# Categorisations of public bodies.
+#
+# Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
+# Email: francis@mysociety.org; WWW: http://www.mysociety.org/
+#
+# $Id: public_body_categories.rb,v 1.1 2009-09-14 14:45:48 francis Exp $
+
+module PublicBodyCategories
+
+    CATEGORIES_WITH_HEADINGS = [
+        "Core EU bodies",
+            [ "european_comission", "European Comission", "European Comission" ],
+        "Executive Agencies",
+            [ "environment", "Environment", "an environmental agency" ], 
+            [ "security_and_defence", "Common Security and Defence", "a security and defence agency" ], 
+            [ "other_agency", "Other agencies", "an agency" ]
+    ]
+
+    # Arranged in different ways for different sorts of displaying
+    CATEGORIES_WITH_DESCRIPTION = CATEGORIES_WITH_HEADINGS.select() { |a| a.instance_of?(Array) } 
+    CATEGORIES = CATEGORIES_WITH_DESCRIPTION.map() { |a| a[0] }
+    CATEGORIES_BY_TAG = Hash[*CATEGORIES_WITH_DESCRIPTION.map() { |a| a[0..1] }.flatten]
+    CATEGORY_SINGULAR_BY_TAG = Hash[*CATEGORIES_WITH_DESCRIPTION.map() { |a| [a[0],a[2]] }.flatten]
+end
+
