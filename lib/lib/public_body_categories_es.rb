c5dbd7e9346db2bd78ed49a064d37437db830bc2
David Cabo <david@calibea.com>
2011-09-20 01:50:28 +0200

    First cut of possible real categories

--- lib/lib/public_body_categories_es.rb
+++
@@ -1,8 +0,0 @@
-PublicBodyCategories.add(:es, [
-    "Core EU bodies",
-        [ "european_comission", "Comisión Europea", "European Comission" ],
-    "Executive Agencies",
-        [ "environment", "Environment", "an environmental agency" ], 
-        [ "security_and_defence", "Common Security and Defence", "a security and defence agency" ], 
-        [ "other_agency", "Other agencies", "an agency" ]
-])
