9a357bf4872b3c58a25b53c4588f3a3412ba7723
David Cabo <david@calibea.com>
2011-09-19 12:28:56 +0200

    Disable theme completely when testing Alaveteli

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -8,13 +8,11 @@ end
 
 # In order to have the theme lib/ folder ahead of the main app one,
 # inspired in Ruby Guides explanation: http://guides.rubyonrails.org/plugins.html
-if ENV["RAILS_ENV"] != "test"
-    %w{ lib }.each do |dir|
-      path = File.join(File.dirname(__FILE__), dir)
-      $LOAD_PATH.insert(0, path)
-      ActiveSupport::Dependencies.autoload_paths << path
-      ActiveSupport::Dependencies.autoload_once_paths.delete(path)
-    end
+%w{ lib }.each do |dir|
+  path = File.join(File.dirname(__FILE__), dir)
+  $LOAD_PATH.insert(0, path)
+  ActiveSupport::Dependencies.autoload_paths << path
+  ActiveSupport::Dependencies.autoload_once_paths.delete(path)
 end
 
 # Define custom controllers
--- lib/lib/config/custom-routes.rb
+++ lib/lib/config/custom-routes.rb
@@ -1,7 +1,7 @@
-ActionController::Routing::Routes.draw do |map|
-    
-    map.with_options :controller => 'AskTheEU' do |controller|
-        controller.frontpage   '/',    :action => 'frontpage'
+if ENV["RAILS_ENV"] != "test"
+    ActionController::Routing::Routes.draw do |map|
+        map.with_options :controller => 'AskTheEU' do |controller|
+            controller.frontpage   '/',    :action => 'frontpage'
+        end
     end
-    
 end
