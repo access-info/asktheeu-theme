db41576fbfa86902e389014b1bbaf952a8a57bea
David Cabo <david@calibea.com>
2011-09-22 04:25:10 +0200

    Create Request Guide help page

--- lib/lib/config/custom-routes.rb
+++ lib/lib/config/custom-routes.rb
@@ -6,6 +6,7 @@ if ENV["RAILS_ENV"] != "test"
 
         map.with_options :controller => 'help' do |help|
             help.help_about_ate '/help/about_ate', :action => 'about_ate'
+            help.help_guide '/help/guide', :action => 'guide'
         end
     end
 end
