07b0716ccbfad4be2bbbfacaa787e9d057bf57d4
David Cabo <david@calibea.com>
2011-09-23 16:58:15 +0200

    Added Right to Know placeholder page

--- lib/lib/config/custom-routes.rb
+++ lib/lib/config/custom-routes.rb
@@ -3,6 +3,7 @@ if ENV["RAILS_ENV"] != "test"
         map.with_options :controller => 'help' do |help|
             help.help_about_ate '/help/about_ate', :action => 'about_ate'
             help.help_guide '/help/guide', :action => 'guide'
+            help.help_right_to_know '/help/right_to_know', :action => 'right_to_know'
         end
     end
 end
