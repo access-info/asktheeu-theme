5db1472a09ba462804c110fcfeefe35e86658158
David Cabo <david@calibea.com>
2011-09-25 14:29:25 +0200

    Rename URL about_ate to about_asktheeu

--- lib/lib/config/custom-routes.rb
+++ lib/lib/config/custom-routes.rb
@@ -1,7 +1,7 @@
 if ENV["RAILS_ENV"] != "test"
     ActionController::Routing::Routes.draw do |map|
         map.with_options :controller => 'help' do |help|
-            help.help_about_ate '/help/about_ate', :action => 'about_ate'
+            help.help_about_asktheeu '/help/about_asktheeu', :action => 'about_asktheeu'
             help.help_guide '/help/guide', :action => 'guide'
             help.help_right_to_know '/help/right_to_know', :action => 'right_to_know'
         end
