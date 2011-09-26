ccc6a52b1fc761357e5bbdce344cff330098d46e
David Cabo <david@calibea.com>
2011-09-26 17:15:38 +0200

    Refactored existing and add new help pages, revised by Helen

--- lib/lib/config/custom-routes.rb
+++ lib/lib/config/custom-routes.rb
@@ -2,6 +2,7 @@ if ENV["RAILS_ENV"] != "test"
     ActionController::Routing::Routes.draw do |map|
         # Additional help pages
         map.with_options :controller => 'help' do |help|
+            help.help_help_out '/help/help_out', :action => 'help_out'
             help.help_guide '/help/guide', :action => 'guide'
             help.help_right_to_know '/help/right_to_know', :action => 'right_to_know'
         end
