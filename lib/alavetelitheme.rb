70ddf475a20432a91a6cfa51ce69d1b030c2e838
David Cabo <david@calibea.com>
2012-02-06 16:59:25 +0100

    fix spelling

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -8,7 +8,7 @@ end
 
 # In order to have the theme lib/ folder ahead of the main app one,
 # inspired in Ruby Guides explanation: http://guides.rubyonrails.org/plugins.html
-%w{ lib }.each do |dir|
+%w{ . }.each do |dir|
   path = File.join(File.dirname(__FILE__), dir)
   $LOAD_PATH.insert(0, path)
   ActiveSupport::Dependencies.autoload_paths << path
