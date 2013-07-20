df7e93f3c7b6c75f2118b5f79a92829df3c08ccf
David Cabo <david.cabo@gmail.com>
2013-07-20 02:26:21 +0200

    Update routes to new Rails 3 format

--- lib/lib/config/custom-routes.rb
+++ lib/lib/config/custom-routes.rb
@@ -1,14 +1,10 @@
 if Rails.env != "test"
-    ActionController::Routing::Routes.draw do |map|
+    Alaveteli::Application.routes.draw do
         # Additional help pages
-        map.with_options :controller => 'help' do |help|
-            help.help_help_out '/help/help_out', :action => 'help_out'
-            help.help_right_to_know '/help/right_to_know', :action => 'right_to_know'
-        end
+        match '/help/help_out' => 'help#help_help_out', :as => 'help_out'
+        match '/help/right_to_know' => 'help#help_right_to_know', :as => 'right_to_know'
         
         # We want to start by showing the public bodies categories and search only
-        map.with_options :controller => 'public_body' do |body|
-            body.body_index "/body/", :action => "index"
-        end
+        match '/body/' => 'public_body#body_index', :as => "index"
     end
 end
