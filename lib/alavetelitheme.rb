445b0e793c86d26a7e25f253c669d10d9a83da77
Jody McIntyre <scjody@modernduck.com>
2014-03-29 18:09:41 -0400

    Update theme to use asset pipeline, for Alaveteli 0.16.

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -25,3 +25,14 @@ require 'gettext_setup.rb'
 
 # Add theme routes
 $alaveteli_route_extensions << 'custom-routes.rb'
+
+# Prepend the asset directories in this theme to the asset path:
+['stylesheets', 'images', 'javascripts'].each do |asset_type|
+    theme_asset_path = File.join(File.dirname(__FILE__),
+                                 '..',
+                                 'assets',
+                                 asset_type)
+    Rails.application.config.assets.paths.unshift theme_asset_path
+end
+
+Rails.application.config.assets.precompile.push 'asktheeu.css'
